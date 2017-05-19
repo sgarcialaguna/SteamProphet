da484f42172f5abfd3a638fa4c04ae60c0f7db41
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-05-19 19:03:20 +0200

    Configure Postgres for Heroku

--- SteamProphet/settings.py
+++ SteamProphet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,6 +85,9 @@ DATABASES = {
         'PASSWORD': 'C1tIcEwl5O0bCQSlYrU7'
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
