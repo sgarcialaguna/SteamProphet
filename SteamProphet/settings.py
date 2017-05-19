cdcc9307946f33e541911e0d3220630f0769e987
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-05-19 18:45:17 +0200

    Port to PostgreSQL

--- SteamProphet/settings.py
+++ SteamProphet/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'SteamProphet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SteamProphet',
+        'USER': 'SteamProphet',
+        'PASSWORD': 'C1tIcEwl5O0bCQSlYrU7'
     }
 }
 
