bc05720221c9d0e0ddcbbe785007ba639d099e96
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-05-19 19:37:25 +0200

    Change createrandomdata

--- SteamProphet/apps/SteamProphet/management/commands/createrandomdata.py
+++ SteamProphet/apps/SteamProphet/management/commands/createrandomdata.py
@@ -22,29 +22,5 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
-        players = [Player.objects.create(name='André'),
-                   Player.objects.create(name='Jochen'),
-                   Player.objects.create(name='Sebastian Stange'),
-                   Player.objects.create(name='Dr. Zoidberg[np]'),
-                   Player.objects.create(name='goschi'),
-                   Player.objects.create(name='Symm'),
-                   Player.objects.create(name='Da Frenz'),
-                   Player.objects.create(name='Varus'),
-                   Player.objects.create(name='Maxi'),
-                   Player.objects.create(name='NobodyJPH'),
-                   Player.objects.create(name='Peter'),
-                   Player.objects.create(name='quod'),
-                   Player.objects.create(name='Seniorenzivi'),
-                   Player.objects.create(name='Darkcloud'),
-                   Player.objects.create(name='Axel'),
-                   Player.objects.create(name='Simon'),
-                   Player.objects.create(name='lnhh'),
-                   Player.objects.create(name='Nebhotep'),
-                   Player.objects.create(name='Peninsula'),
-                   Player.objects.create(name='Nachtfischer'),
-                   Player.objects.create(name='Magier1996'),
-                   Player.objects.create(name='wudu'),
-                   Player.objects.create(name='Vinter'),
-                   Player.objects.create(name='NimaroKun')]
-        for player in players:
+        for player in Player.objects.all():
             assignPicks(player)
