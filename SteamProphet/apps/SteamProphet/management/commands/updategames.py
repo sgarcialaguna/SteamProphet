8d9d987db0d8822170bcef7fb8d320d4e2edb1c2
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-05-24 08:40:33 +0200

    Grab price directly from the Steam API. Explicitly set country code to US so we get the price in USD. Also extract setting price and release date into their own methods.

--- SteamProphet/apps/SteamProphet/management/commands/updategames.py
+++ SteamProphet/apps/SteamProphet/management/commands/updategames.py
@@ -17,25 +17,32 @@ class Command(BaseCommand):
                                     format(game.appID)).json()
             game.players = gameJSON['players_forever']
             game.playersVariance = gameJSON['players_forever_variance']
-            newPrice = gameJSON['price']
-            try:
-                newPrice = float(newPrice)
-                newPrice /= 100.0
-            except (ValueError, TypeError):
-                newPrice = 0.0
-            if game.price != newPrice:
-                if game.price == 0:
-                    game.price = newPrice
-                else:
-                    game.price = min(game.price, newPrice)
-            gameJSON = requests.get('http://store.steampowered.com/api/appdetails/?appids={}&l=english'.
+            gameJSON = requests.get('http://store.steampowered.com/api/appdetails/?appids={}&l=english&cc=us'.
                                     format(game.appID)).json()
-            releaseDateString = gameJSON[str(game.appID)]['data']['release_date']['date']
-            try:
-                game.releaseDate = parse(releaseDateString).date()
-            except ValueError:
-                print('{} has invalid release date {}'.format(game.name, releaseDateString))
-                game.releaseDate = None
+            gameData = gameJSON[str(game.appID)]['data']
+            releaseDateString = gameData['release_date']['date']
+            newPrice = gameData.get('price_overview', {}).get('final', 0)
+            self.setPrice(game, newPrice)
+            self.setReleaseDate(game, releaseDateString)
             game.save()
             # Rate limiter
             time.sleep(0.5)
+
+    def setPrice(self, game, newPrice):
+        try:
+            newPrice = float(newPrice)
+            newPrice /= 100.0
+        except (ValueError, TypeError):
+            newPrice = 0.0
+        if game.price != newPrice:
+            if game.price == 0:
+                game.price = newPrice
+            else:
+                game.price = min(game.price, newPrice)
+
+    def setReleaseDate(self, game, releaseDateString):
+        try:
+            game.releaseDate = parse(releaseDateString).date()
+        except ValueError:
+            print('{} has invalid release date {}'.format(game.name, releaseDateString))
+            game.releaseDate = None
