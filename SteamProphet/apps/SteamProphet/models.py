0af073e01c67ccfa936276b2a9e52487dc65932f
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-07-19 19:54:29 +0200

    Make player <-> user a one to one relation

--- SteamProphet/apps/SteamProphet/models.py
+++ SteamProphet/apps/SteamProphet/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Player(models.Model):
     name = models.CharField(unique=True, max_length=255)
     history = models.TextField(blank=True, null=True)
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User)
 
     def __str__(self):
         return self.name
