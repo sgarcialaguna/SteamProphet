103c33dfd451c09b9a60b4d95cfbeaefa6e721b0
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-07-21 10:42:22 +0200

    Add a dedicated login view and a (German language) privacy policy.

--- SteamProphet/apps/SteamProphet/urls.py
+++ SteamProphet/apps/SteamProphet/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.PlayerListView.as_view(), name='player_list'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+    url(r'^privacy/$', views.PrivacyView.as_view(), name='privacy'),
     url(r'^games/$', views.GameListView.as_view(), name='games_list'),
     url(r'^game/(?P<pk>\d+)/$', views.GameDetailView.as_view(), name='game'),
     url(r'^player/(?P<pk>\d+)/$', views.PlayerDetailView.as_view(), name='player'),
     url(r'^delete_game/(?P<pk>\d+)/$', views.DeleteGameView.as_view(), name='delete_game'),
     url(r'^create_picks/$', views.CreatePicksView.as_view(), name='create_picks'),
-    url(r'^player_profile', views.PlayerProfileView.as_view(), name='player_profile')
+    url(r'^player_profile/', views.PlayerProfileView.as_view(), name='player_profile')
 ]
