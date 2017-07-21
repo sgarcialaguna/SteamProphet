1d90272083957eac80e41fda31a3dc2838c142f2
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-07-21 17:42:24 +0200

    Handle error on updategames

--- SteamProphet/apps/SteamProphet/management/commands/updategames.py
+++ SteamProphet/apps/SteamProphet/management/commands/updategames.py
@@ -18,23 +18,26 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         for game in Game.objects.filter(matured=False):
-            gameJSON = requests.get('https://steamspy.com/api.php?request=appdetails&appid={}'.
-                                    format(game.appID)).json()
-            game.players = gameJSON['players_forever']
-            game.playersVariance = gameJSON['players_forever_variance']
-            gameJSON = requests.get('http://store.steampowered.com/api/appdetails/?appids={}&l=english&cc=us'.
-                                    format(game.appID)).json()
-            gameData = gameJSON[str(game.appID)]['data']
-            game.name = gameData['name']
-            releaseDateString = gameData['release_date']['date']
-            newPrice = gameData.get('price_overview', {}).get('final', 0)
-            self.setPrice(game, newPrice)
-            self.setReleaseDate(game, releaseDateString)
-            self.setMaturedFlag(game, gameData)
-            self.saveHistory(game)
-            game.save()
-            # Rate limiter
-            time.sleep(0.5)
+            try:
+                gameJSON = requests.get('https://steamspy.com/api.php?request=appdetails&appid={}'.
+                                        format(game.appID)).json()
+                game.players = gameJSON['players_forever']
+                game.playersVariance = gameJSON['players_forever_variance']
+                gameJSON = requests.get('http://store.steampowered.com/api/appdetails/?appids={}&l=english&cc=us'.
+                                        format(game.appID)).json()
+                gameData = gameJSON[str(game.appID)]['data']
+                game.name = gameData['name']
+                releaseDateString = gameData['release_date']['date']
+                newPrice = gameData.get('price_overview', {}).get('final', 0)
+                self.setPrice(game, newPrice)
+                self.setReleaseDate(game, releaseDateString)
+                self.setMaturedFlag(game, gameData)
+                self.saveHistory(game)
+                game.save()
+                # Rate limiter
+                time.sleep(0.5)
+            except:
+                print('Could not update game ' + game.appID)
 
         for player in Player.objects.all():
             self.savePlayerHistory(player)
