13f45c4c15317d2c348c01f2e12e7b647915faa7
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-08-11 18:14:39 +0200

    Improbe printupcominggames

--- SteamProphet/apps/SteamProphet/management/commands/printupcominggames.py
+++ SteamProphet/apps/SteamProphet/management/commands/printupcominggames.py
@@ -1,15 +1,18 @@
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
-from SteamProphet.apps.SteamProphet.models import Game
+from SteamProphet.apps.SteamProphet.models import Game, Week
 
 
 class Command(BaseCommand):
     help = 'Print the upcoming games for the forum'
 
+    def getWeek(self):
+        for week in Week.objects.order_by('-week'):
+            if week.game_set.exists():
+                return week
+
     @transaction.atomic
     def handle(self, *args, **options):
-        # week = Game.objects.order_by('-week__week').first().week.first()
-        # games = Game.objects.filter(week=week)
-        for game in Game.objects.all():
+        for game in Game.objects.all(week=self.getWeek()):
             print('[URL=https://store.steampowered.com/app/{}/]{}[/URL]'.format(game.appID, game.name))
