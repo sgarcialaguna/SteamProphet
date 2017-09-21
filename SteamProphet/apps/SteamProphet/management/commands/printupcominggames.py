fad978760ce8d749b4c61e538a1fbe62192c99e3
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-09-21 19:07:13 +0200

    printupcominggames additionally prints number of followers. Games are sorted descending by number of followers.

--- SteamProphet/apps/SteamProphet/management/commands/printupcominggames.py
+++ SteamProphet/apps/SteamProphet/management/commands/printupcominggames.py
@@ -1,7 +1,23 @@
+from operator import attrgetter
+
+import requests
+from bs4 import BeautifulSoup
 from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from SteamProphet.apps.SteamProphet.models import Game, Week
+
+
+def getFollowers(game):
+    text = requests.get('https://steamspy.com/app/{}'.format(game.appID)).text
+    soup = BeautifulSoup(text, 'html5lib')
+    strings = soup.stripped_strings
+    for s in strings:
+        if s == 'Followers':
+            # example: ": 34,586
+            return int(next(strings)[2:].replace(',', ''))
+
+    return 0
 
 
 class Command(BaseCommand):
@@ -14,5 +30,15 @@ class Command(BaseCommand):
 
     @transaction.atomic
     def handle(self, *args, **options):
+        games = []
         for game in Game.objects.filter(week=self.getWeek()):
-            print('[URL=https://store.steampowered.com/app/{}/]{}[/URL]'.format(game.appID, game.name))
+            game.url = 'https://store.steampowered.com/app/{}/{}'.format(game.appID, game.name)
+            game.followers = getFollowers(game)
+            games.append(game)
+        games = sorted(games, key=attrgetter('followers'), reverse=True)
+        for game in games:
+            print('[URL="{url}"]{name}[/URL] ({followers})'.format(
+                url=game.url,
+                name=game.name,
+                followers=game.followers
+            ))
