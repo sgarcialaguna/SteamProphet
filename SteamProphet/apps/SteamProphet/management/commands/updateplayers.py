051493052be3f911dad687b4f1b7829c05b40333
Santiago Garcia <sgarcialaguna@googlemail.com>
2017-10-07 08:50:36 +0200

    Move function out of class

--- SteamProphet/apps/SteamProphet/management/commands/updateplayers.py
+++ SteamProphet/apps/SteamProphet/management/commands/updateplayers.py
@@ -9,28 +9,29 @@ from SteamProphet.apps.SteamProphet import services
 from SteamProphet.apps.SteamProphet.models import Player
 
 
+def savePlayerHistory(player):
+    if player.history:
+        history = json.loads(player.history)
+    else:
+        history = []
+
+    timestamp = calendar.timegm(now().date().timetuple())
+    latestEntry = {
+        'score': services.computePlayerScore(player),
+        'timestamp': timestamp
+    }
+    if history and history[-1]['timestamp'] == timestamp:
+        history[-1] = latestEntry
+    else:
+        history.append(latestEntry)
+    player.history = json.dumps(history)
+
+
 class Command(BaseCommand):
     help = 'Updates all players'
 
     @transaction.atomic
     def handle(self, *args, **options):
         for player in Player.objects.all():
-            self.savePlayerHistory(player)
+            savePlayerHistory(player)
             player.save()
-
-    def savePlayerHistory(self, player):
-        if player.history:
-            history = json.loads(player.history)
-        else:
-            history = []
-
-        timestamp = calendar.timegm(now().date().timetuple())
-        latestEntry = {
-            'score': services.computePlayerScore(player),
-            'timestamp': timestamp
-        }
-        if history and history[-1]['timestamp'] == timestamp:
-            history[-1] = latestEntry
-        else:
-            history.append(latestEntry)
-        player.history = json.dumps(history)
